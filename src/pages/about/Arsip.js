fca85e5a3cde126efca1a0a4ebade5ed5db3151a
twinedo <twinedo@github.com>
2021-02-23 14:48:15 +0700

    buat screen arsip

---
+++ src/pages/about/Arsip.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './arsip.scss';
+
+const Arsip = () => {
+    return (
+        <div>
+            <div className="wrapper">
+                <div className="col-lg-12 col-md-6 col-sm-6">teset</div>
+                <div className="col-lg-12 col-md-6 col-sm-6">teset</div>
+            </div>
+        </div>
+    )
+}
+
+export default Arsip;
